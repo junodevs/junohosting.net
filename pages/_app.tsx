c391daedfe2102a8fc7f481cf9e2071974426c1d
Luke Whrit <lukewhrit@gmail.com>
2021-04-07 15:06:57 -0400

    build(yarn): mass dependency upgrades

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@
 */
 
 import { AppProps } from 'next/app'
-import 'tailwindcss/tailwind.css';
+import 'tailwindcss/tailwind.css'
 
 export default function MyApp ({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
